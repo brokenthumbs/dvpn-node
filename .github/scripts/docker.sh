226d877d7263c4a044e0c6f9bf15b4d602eba6da
Dan Chan <dan.chan@january.com>
2024-01-14 21:45:14 -0500

    wip

--- .github/scripts/docker.sh
+++ .github/scripts/docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --tag dvpn-node:latest
+docker build --tag dvpn-node:latest .
 docker images
 
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
